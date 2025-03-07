3f332d5a897d1e82c8a888196384c3d11a80dd54
codeneat9 <yashwithbehara18@gmail.com>
2025-03-07 16:28:45 +0530

    Cylinder constructor

--- Cylinder.java
+++ Cylinder.java
@@ -3,3 +3,5 @@ import java.util.Scanner;
 // Cylinder class is a subclass of Shape class and implements Volume interface
 class Cylinder extends Shape implements Volume {
     double radius, height;
+    // Constructor
+    Cylinder() { super("Cylinder"); }
