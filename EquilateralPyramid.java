0d58a660900dd42c0a7118b1053d4344bafcbf48
codeneat9 <yashwithbehara18@gmail.com>
2025-03-07 17:25:04 +0530

    Constructor Equilateral Pyramid

--- EquilateralPyramid.java
+++ EquilateralPyramid.java
@@ -3,3 +3,5 @@ import java.util.Scanner;
 // EquilateralPyramid class is a subclass of Shape class and implements Volume interface
 class EquilateralPyramid extends Shape implements Volume {
     double base, height;
+    // Constructor
+    EquilateralPyramid() { super("Equilateral Pyramid"); }
