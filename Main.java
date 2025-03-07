7aed26625f07dae2b71d5f11dc8cf025f08df129
codeneat9 <yashwithbehara18@gmail.com>
2025-03-07 16:21:50 +0530

    Creating Main class

--- Main.java
+++ Main.java
@@ -3,3 +3,8 @@
 //23070126151
 //AIML B3
 import java.util.Scanner;
+// Main class to test the Shape, Circle, Rectangle, Square, Sphere, Cylinder, EquilateralPyramid classes
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int choice;
