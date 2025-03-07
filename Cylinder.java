edf992ee2178588373d19126b0800e8a7e626368
codeneat9 <yashwithbehara18@gmail.com>
2025-03-07 16:28:32 +0530

    Cylinder class extends Shape class

--- Cylinder.java
+++ Cylinder.java
@@ -1,2 +1,5 @@
 // Cylinder.java
 import java.util.Scanner;
+// Cylinder class is a subclass of Shape class and implements Volume interface
+class Cylinder extends Shape implements Volume {
+    double radius, height;
