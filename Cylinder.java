912260d1a1c26794c4a037ee804afad7b1282ab8
codeneat9 <yashwithbehara18@gmail.com>
2025-03-07 16:29:03 +0530

    Taking radius and height as input

--- Cylinder.java
+++ Cylinder.java
@@ -5,3 +5,11 @@ class Cylinder extends Shape implements Volume {
     double radius, height;
     // Constructor
     Cylinder() { super("Cylinder"); }
+    // Overriding the getInput method
+    void getInput() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter radius: ");
+        radius = sc.nextDouble();
+        System.out.print("Enter height: ");
+        height = sc.nextDouble();
+    }
