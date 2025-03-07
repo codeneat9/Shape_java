76e94ebb87ad771810826bb070fcc1ec1fadf0d8
codeneat9 <yashwithbehara18@gmail.com>
2025-03-07 17:29:25 +0530

    Constructor rectangle

--- Rectangle.java
+++ Rectangle.java
@@ -3,3 +3,5 @@ import java.util.Scanner;
 // Rectangle class is a subclass of Shape class
 class Rectangle extends Shape {
     double length, width;
+    // Constructor
+    Rectangle() { super("Rectangle"); }
