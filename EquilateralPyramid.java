2acdf29a3793d9c406bc14dad67aab38b61ecc21
codeneat9 <yashwithbehara18@gmail.com>
2025-03-07 17:25:31 +0530

    inputting Base, height of pyramid

--- EquilateralPyramid.java
+++ EquilateralPyramid.java
@@ -5,3 +5,11 @@ class EquilateralPyramid extends Shape implements Volume {
     double base, height;
     // Constructor
     EquilateralPyramid() { super("Equilateral Pyramid"); }
+    // Overriding the getInput method
+    void getInput() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter base side: ");
+        base = sc.nextDouble();
+        System.out.print("Enter height: ");
+        height = sc.nextDouble();
+    }
