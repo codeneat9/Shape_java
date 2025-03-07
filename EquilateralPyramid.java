324764993e2662ce769b74cbbdbb2f0fe7a50ed0
codeneat9 <yashwithbehara18@gmail.com>
2025-03-07 17:09:24 +0530

    Equilateral pyramid extends shape class

--- EquilateralPyramid.java
+++ EquilateralPyramid.java
@@ -1,2 +1,5 @@
 // Equilateral Pyramid.java
 import java.util.Scanner;
+// EquilateralPyramid class is a subclass of Shape class and implements Volume interface
+class EquilateralPyramid extends Shape implements Volume {
+    double base, height;
