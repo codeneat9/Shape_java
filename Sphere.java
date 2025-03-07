7f362f0b07c2b44aabac76bbe8215fecf5de374a
codeneat9 <yashwithbehara18@gmail.com>
2025-03-07 17:33:31 +0530

    Sphere extends Shape class

--- Sphere.java
+++ Sphere.java
@@ -1,2 +1,5 @@
 // Sphere.java
 import java.util.Scanner;
+// Sphere class is a subclass of Shape class and implements Volume interface
+class Sphere extends Shape implements Volume {
+    double radius;
