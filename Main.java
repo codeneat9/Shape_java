9272a46a056ff59c51021f1915ae4196b626a219
codeneat9 <yashwithbehara18@gmail.com>
2025-03-07 16:23:00 +0530

    To display the menu

--- Main.java
+++ Main.java
@@ -8,3 +8,15 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int choice;
+        // Display the menu until the user chooses to exit
+        do {
+            System.out.println("\n--- Select Shape ---");
+            System.out.println("1. Circle");
+            System.out.println("2. Rectangle");
+            System.out.println("3. Square");
+            System.out.println("4. Sphere");
+            System.out.println("5. Cylinder");
+            System.out.println("6. Equilateral Pyramid");
+            System.out.println("0. Exit");
+            System.out.print("Enter your choice: ");
+            choice = sc.nextInt();
